f5b64f45c90b2b6c034a06570b07dae1f3086331
soongu <naong526@naver.com>
2025-07-11 12:48:11 +0900

    Initial commit

---
+++ src/main/java/com/spring/basic/SpringWebBasic202507Application.java
@@ -0,0 +1,13 @@
+package com.spring.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebBasic202507Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebBasic202507Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/basic/SpringWebBasic202507ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebBasic202507ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
